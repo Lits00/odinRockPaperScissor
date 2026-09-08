6a82527ecfb2dbac1cc66f60b86aa53704b20794
Lits <106635891+Lits00@users.noreply.github.com>
2026-09-08 20:14:33 +0800

    Created computer choice function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let computerChoice = "";
+    randNum = Math.floor(Math.random() * 3)
+    console.log(randNum)
+    switch (randNum) {
+        case 0:
+            computerChoice = "rock";
+        break;
+        case 1:
+            computerChoice = "paper";
+        break;
+        case 2:
+            computerChoice = "scissor";
+        break;
+    }
+    return computerChoice;
+}
